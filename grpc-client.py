e160221d7764a0fec8ed70dd5700c96ebb45bc16
sunqc666 <72462145+sunqc666@users.noreply.github.com>
2023-07-04 16:10:53 +0800

    Update and rename grpc-client to grpc-client.py
    update

--- grpc-client.py
+++ grpc-client.py
@@ -6,10 +6,10 @@ _HOST = '127.0.0.1'
 _PORT = '19999'
 
 
-def main():
+def main(param):
     with grpc.insecure_channel("{0}:{1}".format(_HOST, _PORT)) as channel:
         client = compute_pb2_grpc.ComputeStub(channel=channel)
-        response = client.SayHello(compute_pb2.HelloRequest(helloworld="123456"))
+        response = client.SayHello(compute_pb2.HelloRequest(**param))
     print("received: " + response.result)
 
 
